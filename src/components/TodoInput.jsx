98196a0e9a7b34c82f54ce4c5d76fdf77e472ea3
Rahul Wagadre <84171564+Rahulwagadre@users.noreply.github.com>
2024-11-19 21:11:57 +0530

    Dev (#2)
    * added new components
    
    * implemented Todo input comp (#1)

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TodoInput = () => {
+    return (
+        <header>
+            <input placeholder="Enter Todo..."></input>
+            <button>Add</button>
+        </header>
+    )
+}
