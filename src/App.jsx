deb8723e0bdfd628c32ab9faeebebd2e4a3d0bcf
Rahul Wagadre <84171564+Rahulwagadre@users.noreply.github.com>
2024-11-19 23:59:18 +0530

    Dev (#4)
    * added new components
    
    * implemented Todo input comp (#1)
    
    * implementation of component functionalities (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [inputTodo, setInputTodo] = useState('')
+  
+  const handleAddTodo = (newTodo) => {
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  }
+
+  const handleUpdateTodo = (index) => {
+    const toBeEditedTodo = todos[index];
+    setInputTodo(toBeEditedTodo);
+    handleDeleteTodo(index);
+  }
+
+  const handleDeleteTodo = (deleteIndex) => {
+    const newTodos = todos.filter((todo, index) => {
+      return deleteIndex !== index;
+    })
+    setTodos(newTodos);
+  }
+
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    <>
+      <TodoInput handleAddTodo = {handleAddTodo} inputTodo = {inputTodo} setInputTodo = {setInputTodo} />
+      <TodoList todos = {todos} handleDeleteTodo = {handleDeleteTodo} handleUpdateTodo = {handleUpdateTodo}/>
+    </>
   )
 }
 
