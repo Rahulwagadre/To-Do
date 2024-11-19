050f69cbd4e7fa70d85bf134bd819dd87bf9483a
Rahul Wagadre <84171564+Rahulwagadre@users.noreply.github.com>
2024-11-20 00:43:05 +0530

    final commit (#7)

--- src/components/Todocard.jsx
+++ src/components/Todocard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function Todocard() {
+export default function Todocard({index, handleDeleteTodo, handleUpdateTodo, children}) {
     return (
-        <div>
-            Todocard
-        </div>
+        <li className='todoItem' >
+            {children}
+            <div className='actionsContainer'>
+                <button onClick={() => {
+                    handleUpdateTodo(index)
+                }}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={() => {
+                    handleDeleteTodo(index)
+                }}>
+                    <i className="fa-regular fa-trash-can"></i>
+                </button>
+            </div>
+        </li>
     )
 }
